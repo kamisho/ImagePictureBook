ddd5a8d85946c9a4891fe32e76299aa7e8dff7a4
kamisho <kamisho.message@icloud.com>
2019-01-06 17:27:11 +0900

    developブランチのmergeと環境変更

--- firebase.js
+++ firebase.js
@@ -3,20 +3,20 @@ import firestore from 'firebase/firestore';
 
 const config = {
   // 本番環境
-	// apiKey: "AIzaSyBRpBvMGOJV6-P0MKy2gg6XoPhguuDH_Iw",
-  // authDomain: "bijyo-collection.firebaseapp.com",
-  // databaseURL: "https://bijyo-collection.firebaseio.com",
-  // projectId: "bijyo-collection",
-  // storageBucket: "bijyo-collection.appspot.com",
-  // messagingSenderId: "684500278423"
+	apiKey: "AIzaSyBRpBvMGOJV6-P0MKy2gg6XoPhguuDH_Iw",
+  authDomain: "bijyo-collection.firebaseapp.com",
+  databaseURL: "https://bijyo-collection.firebaseio.com",
+  projectId: "bijyo-collection",
+  storageBucket: "bijyo-collection.appspot.com",
+  messagingSenderId: "684500278423"
 
   // 開発環境
-    apiKey: "AIzaSyAAqchjetUGTnbr4S99MSiZdauUoWtFk0g",
-    authDomain: "bijostagram-test.firebaseapp.com",
-    databaseURL: "https://bijostagram-test.firebaseio.com",
-    projectId: "bijostagram-test",
-    storageBucket: "bijostagram-test.appspot.com",
-    messagingSenderId: "913422222238"
+  // apiKey: "AIzaSyAAqchjetUGTnbr4S99MSiZdauUoWtFk0g",
+  // authDomain: "bijostagram-test.firebaseapp.com",
+  // databaseURL: "https://bijostagram-test.firebaseio.com",
+  // projectId: "bijostagram-test",
+  // storageBucket: "bijostagram-test.appspot.com",
+  // messagingSenderId: "913422222238"
 }
 
 // firebaseのインスタンスを初期化する
